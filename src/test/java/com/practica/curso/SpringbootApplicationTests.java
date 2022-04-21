d44ac1b6b73dca36509fc2aa1729e7846c6830f6
cesar <cesarfigueroa388@gmail.com>
2022-04-21 16:08:26 +0200

    Ultimos cambios en el proyecto

---
+++ src/test/java/com/practica/curso/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
