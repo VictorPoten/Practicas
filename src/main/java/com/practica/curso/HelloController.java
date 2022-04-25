13ac59b1bed689fa6e272ae9ec7d7cbe8a1b0c82
cesar <cesarfigueroa388@gmail.com>
2022-04-25 19:20:25 +0200

    Version del dia 25 de Abril

--- src/main/java/com/practica/curso/HelloController.java
+++ src/main/java/com/practica/curso/HelloController.java
@@ -1,7 +1,10 @@
 package com.practica.curso;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class HelloController {
@@ -10,5 +13,21 @@ public class HelloController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "Welcome user";
+	}
+	
+	@GetMapping("/user/{name}")
+	public String name(@PathVariable String na) {
+		return "Welcome " + na;
+	}
+	
+	@PostMapping("/user/{name}")
+	public String name2(@RequestBody String na) {
+		return "Welcome " + na;
+	}
+
 
 }
